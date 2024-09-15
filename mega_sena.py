aca18fbe4ff721b7527537c229dacbe56c3aa040
Fernando Kavinsky <fernandosdba@gmail.com>
2024-09-15 15:26:18 -0300

    Update mega_sena.py
    Upgrade

--- mega_sena.py
+++ mega_sena.py
@@ -1,8 +1,9 @@
 from random import randint
-a = [randint (1,60),randint (1,60),randint (1,60), randint (1,60),randint (1,60) ,randint (1,60)]
-b = []
-for value in a:
-    while value in b:
-        value = randint(1, 6)
-    b.append(value)
-print (b)
+
+b = set()
+
+while len(b) < 6:
+    b.add(randint(1, 60))
+
+b = list(b)
+print(b)
